d8ab6817663178e92fb8689a2c0e5cd3791c81c5
zhoulei <zhouleisjj@163.com>
2023-09-26 19:50:23 +0800

    排除首页和auth2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -110,7 +110,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     }
   }, [])
 
-  if (!accessToken) {
+  if (!accessToken && pathname != "/" && pathname != "/auth2/") {
     return (
       <html lang="en" id="_next">
         <meta name="version" content="1.0.0" />
